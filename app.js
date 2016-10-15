fd62e3939f3c1172c06560716456264ba37ad7d6
Murilo Nogueira de Avila <murilo.avila068@gmail.com>
2016-10-15 19:58:46 -0300

    aula 2.2 feita

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var app = require('./config/express')();
 
-app.get('/produtos', function(request, response){
-	response.render('produtos/lista');
-});
+var routesProdutos = require('./app/routes/produtos')(app);
 
 app.listen(3000, function(){
 	console.log("Servidor rodando");

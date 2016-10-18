fa987cfa6b5335bd63e98c68c480e1468e76149a
Murilo Nogueira de Avila <murilo.avila068@gmail.com>
2016-10-17 23:36:59 -0200

    aula 3

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,19 @@
 module.exports = function(app){
 	app.get('/produtos', function(request, response){
-		response.render('produtos/lista');
+		var mysql = require('mysql');
+		var connection = mysql.createConnection({
+			host : 'localhost',
+			user : 'root',
+			password : '',
+			database : 'casadocodigo_nodejs'
+		});
+
+		connection.query('select * from livros', function(err, result){
+			response.send(result);
+		});
+
+
+
+		connection.end();
 	});
 }

e6ef876a85a7ab714480723205485314703a626f
Murilo Nogueira de Avila <murilo.avila068@gmail.com>
2016-10-26 23:49:11 -0200

    aula 7.2

---
+++ test/produtos.js
@@ -0,0 +1,24 @@
+var http = require('http');
+
+describe('ProdutosController', function(){
+    it('#listagem json', function(done){
+        var configuracoes = {
+            hostname: 'localhost',
+            port: 3000,
+            path: '/produtos',
+            headers: {
+                'Accept':'application/json'
+            }
+        };
+
+        http.get(configuracoes, function(res){
+            if(res.statusCode == 200){
+                console.log("Status OK");
+            }
+            if(res.headers['content-type'] == 'application/json; charset=utf-8'){
+                console.log("content-type ok");
+            }
+            done();
+        });
+    });
+});
